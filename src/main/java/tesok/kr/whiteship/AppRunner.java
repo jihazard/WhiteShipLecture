dcebdf07de32baa06c0b547ad7384b4f0b80cef1
jihazard <yoonjh238@gmail.com>
2019-04-13 13:47:11 +0900

    프로퍼티 설정 및 호출

--- src/main/java/tesok/kr/whiteship/AppRunner.java
+++ src/main/java/tesok/kr/whiteship/AppRunner.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.Properties;
 
 @Component
 public class AppRunner implements ApplicationRunner {
@@ -18,10 +18,14 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     BookRepository bookRepository;
 
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment= ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+         System.out.println(environment.getProperty("app.name")); //환경 변수불러오기
+        System.out.println(environment.getProperty("app.info")); //환경 변수불러오기
+        System.out.println(environment.getProperty("app.test")); //환경 변수불러오기
+
     }
 }
