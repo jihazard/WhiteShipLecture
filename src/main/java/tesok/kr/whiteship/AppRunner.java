02258bd29b5c6ea9b18cbe7f35028ef961deaea1
jihazard <yoonjh238@gmail.com>
2019-04-13 15:45:56 +0900

    resourceLoader

--- src/main/java/tesok/kr/whiteship/AppRunner.java
+++ src/main/java/tesok/kr/whiteship/AppRunner.java
@@ -3,20 +3,22 @@ package tesok.kr.whiteship;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
-    ApplicationEventPublisher applicationEventPublisher;
+    ResourceLoader resourceLoader;
 
-    @Autowired
-    MyEventHandler handler;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        applicationEventPublisher.publishEvent(new MyEvent(this,100));
+
+        Resource res = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(res.exists());
+        System.out.println(res.getDescription());
 
     }
 }
