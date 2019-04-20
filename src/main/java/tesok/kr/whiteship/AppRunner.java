cc457a493298f21bce2d95b524646187c8d5a9ca
jihazard <yoonjh238@gmail.com>
2019-04-20 17:18:45 +0900

    valdator

--- src/main/java/tesok/kr/whiteship/AppRunner.java
+++ src/main/java/tesok/kr/whiteship/AppRunner.java
@@ -6,19 +6,41 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
     ResourceLoader resourceLoader;
 
+    @Autowired
+    Validator validator;
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("호출 클래스 : " + validator.getClass());
 
-        Resource res = resourceLoader.getResource("classpath:test.txt");
-        System.out.println(res.exists());
-        System.out.println(res.getDescription());
+        Book book = new Book();
+        Errors errors = new BeanPropertyBindingResult(book,"book");
+
+        validator.validate(book,errors);
+
+        errors.getAllErrors().forEach(e ->{
+                    System.out.println(e.getDefaultMessage());
+                    Arrays.stream(e.getCodes()).forEach(code-> {
+                        System.out.println("받은에러 코드 : " + code);
+
+                    });
+
+                }
+        );
+
+
 
     }
 }
